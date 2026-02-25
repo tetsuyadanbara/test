32006bdc7c8588c92c7745ebb470047546076890
tetsuyadanbara <39230388+tetsuyadanbara@users.noreply.github.com>
2026-02-25 13:37:36 +0900

    Update RB.js

--- RB.js
+++ RB.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         donguri arena assist tool - Hybrid Version
-// @version      1.4.0
-// @description  コード1をベースにコード2の自動参戦・自動装備機能を統合
-// @author       7234e634 & ぱふぱふ (Merged)
+// @name         donguri arena assist tool
+// @version      1.4 (Merged & Updated)
+// @description  fixes and additions (Auto Join, Auto Equip, New Grid Spec)
+// @author       Merged
 // @match        https://donguri.5ch.net/teambattle*
 // @match        https://donguri.5ch.net/bag
 // ==/UserScript==
@@ -28,16 +28,24 @@
     return;
   }
 
-  // --- コード2: モード判定の統合 ---
   const MODEQ = location.search.slice(1);
-  let MODENAME = '[ﾊｰﾄﾞｺｱ]';
-  let MODEM = 'hc';
+
+  let MODENAME;
   if (MODEQ.includes('m=l')) {
       MODENAME = '[ﾗﾀﾞｰ]';
-      MODEM = 'l';
   } else if (MODEQ.includes('m=rb')) {
       MODENAME = '[ﾚﾄﾞﾌﾞﾙ]';
+  } else {
+      MODENAME = '[ﾊｰﾄﾞｺｱ]';
+  }
+
+  let MODEM;
+  if (MODEQ.includes('m=l')) {
+      MODEM = 'l';
+  } else if (MODEQ.includes('m=rb')) {
       MODEM = 'rb';
+  } else {
+      MODEM = 'hc';
   }
 
   const vw = Math.min(document.documentElement.clientWidth, window.innerWidth || 0);
@@ -79,21 +87,44 @@
       toolbar.style.right = distance;
     }
   })();
+  
   const h4 = header.querySelector('h4');
   if(h4) h4.style.display = 'none';
   header.append(toolbar);
   const progressBarContainer = document.createElement('div');
+  const progressBar = document.createElement('div');
+  const progressBarBody = document.createElement('div');
+  const progressBarInfo = document.createElement('p');
+  progressBar.classList.add('progress-bar');
+  progressBar.style.display = 'inline-block';
+  progressBar.style.width = '400px';
+  progressBar.style.maxWidth = '100vw';
+  progressBar.style.height = '20px';
+  progressBar.style.background = '#ccc';
+  progressBar.style.borderRadius = '8px';
+  progressBar.style.fontSize = '16px';
+  progressBar.style.overflow = 'hidden';
+  progressBar.style.marginTop = '5px';
+  progressBarBody.style.height = '100%';
+  progressBarBody.style.lineHeight = 'normal';
+  progressBarBody.style.background = '#428bca';
+  progressBarBody.style.textAlign = 'right';
+  progressBarBody.style.paddingRight = '5px';
+  progressBarBody.style.boxSizing = 'border-box';
+  progressBarBody.style.color = 'white';
+  progressBarInfo.style.marginTop = '0';
+  progressBarInfo.style.marginBottom = '0';
+  progressBarInfo.style.overflow = 'auto';
+  progressBarInfo.style.whiteSpace = 'nowrap';
+
+  progressBarContainer.append(progressBarInfo,progressBar);
+  progressBar.append(progressBarBody)
   toolbar.append(progressBarContainer);
 
-  // コード2ベースのプログレスバー要素初期化
-  const progressBarBodyInfo = document.createElement('p');
-  progressBarBodyInfo.style.margin = '0';
-  progressBarBodyInfo.style.fontSize = '14px';
-
-  let shouldSkipAreaInfo, shouldSkipAutoEquip, cellSelectorActivate, rangeAttackProcessing;
-  let currentPeriod, currentProgress;
+  // add buttons and select to custom menu
+  let shouldSkipAreaInfo, shouldSkipAutoEquip, cellSelectorActivate, rangeAttackProcessing,
+      currentPeriod, currentProgress;
   let currentEquipName = '';
-
   (()=>{
     const button = document.createElement('button');
     button.type = 'button';
@@ -105,6 +136,7 @@
     button.style.padding = '2px';
     button.style.width = '6em';
     button.style.fontSize = '65%';
+    button.style.border = 'none';
 
     if (vw < 768) {
       progressBarContainer.style.fontSize = '60%';
@@ -116,12 +148,18 @@
       const isSubMenuOpen = subMenu.style.display === 'flex';
       subMenu.style.display = isSubMenuOpen ? 'none' : 'flex';
     })
-  
+
     const equipButton = button.cloneNode();
     equipButton.textContent = '■装備';
     equipButton.addEventListener('click', ()=>{
       panel.style.display = 'flex';
     });
+
+    const toggleViewButton = button.cloneNode();
+    toggleViewButton.innerText = '表示\n切り替え';
+    toggleViewButton.addEventListener('click', ()=>{
+      toggleCellViewMode();
+    })
 
     let currnetSort = 'default';
     const sortButton = button.cloneNode();
@@ -129,3 +167,347 @@
     sortButton.addEventListener('click', ()=>{
       if(currnetSort === 'default') {
         sortCells('cond');
+        currnetSort = 'cond';
+      } else {
+        sortCells('default');
+        currnetSort = 'default';
+      }
+    })
+
+    const refreshButton = button.cloneNode();
+    refreshButton.innerText = 'エリア情報\n更新';
+    refreshButton.addEventListener('click',()=>{
+      fetchAreaInfo(false);
+    });
+
+    const skipAreaInfoButton = button.cloneNode();
+    skipAreaInfoButton.innerText = 'セル情報\nスキップ';
+    skipAreaInfoButton.style.color = '#fff';
+    if (settings.skipArenaInfo) {
+      skipAreaInfoButton.style.background = '#46f';
+      shouldSkipAreaInfo = true;
+    } else {
+      skipAreaInfoButton.style.background = '#888';
+      shouldSkipAreaInfo = false;
+    }
+    skipAreaInfoButton.addEventListener('click', ()=>{
+      if(shouldSkipAreaInfo) {
+        skipAreaInfoButton.style.background = '#888';
+        shouldSkipAreaInfo = false;
+      } else {
+        skipAreaInfoButton.style.background = '#46f';
+        shouldSkipAreaInfo = true;
+      }
+      settings.skipArenaInfo = shouldSkipAreaInfo;
+      localStorage.setItem('aat_settings', JSON.stringify(settings));
+    });
+
+    const subMenu = document.createElement('div');
+    subMenu.style.display = 'none';
+    subMenu.style.flexWrap = 'nowrap';
+    subMenu.style.overflowX = 'hidden';
+    subMenu.style.position = 'relative';
+
+    (()=>{
+      const subButton = button.cloneNode();
+      subButton.style.fontSize = '65%';
+      subButton.style.width = '6em';
+      subButton.style.border = 'none';
+      subButton.style.padding = '2px';
+
+      const div = document.createElement('div');
+      div.style.display = 'flex';
+      div.style.flex = '1';
+      div.style.justifyContent = 'center';
+      div.style.gap = '2px';
+      div.style.overflowX = 'auto';
+      div.style.height = '100%';
+
+      const cellButton = subButton.cloneNode();
+      cellButton.innerText = 'エリア情報\n再取得';
+      cellButton.addEventListener('click',()=>{
+        fetchAreaInfo(true);
+      });
+
+      const skipAutoEquipButton = subButton.cloneNode();
+      skipAutoEquipButton.textContent = '自動装備';
+      skipAutoEquipButton.style.color = '#fff';
+      skipAutoEquipButton.classList.add('skip-auto-equip');
+      if (settings.skipAutoEquip) {
+        skipAutoEquipButton.style.background = '#888';
+        shouldSkipAutoEquip = true;
+      } else {
+        skipAutoEquipButton.style.background = '#46f';
+        shouldSkipAutoEquip = false;
+      }
+      skipAutoEquipButton.addEventListener('click', ()=>{
+        if (shouldSkipAutoEquip) {
+          skipAutoEquipButton.style.background = '#46f';
+          shouldSkipAutoEquip = false;
+        } else {
+          skipAutoEquipButton.style.background = '#888';
+          shouldSkipAutoEquip = true;
+        }
+        settings.skipAutoEquip = shouldSkipAutoEquip;
+        localStorage.setItem('aat_settings', JSON.stringify(settings));
+      });
+
+      const slideMenu = document.createElement('div');
+      slideMenu.style.display = 'flex';
+      slideMenu.style.flex = '1';
+      slideMenu.style.justifyContent = 'center';
+      slideMenu.style.gap = '2px';
+      slideMenu.style.position = 'absolute';
+      slideMenu.style.width = '100%';
+      slideMenu.style.height = '100%';
+      slideMenu.style.right = '-100%';
+      slideMenu.style.background = '#fff';
+      slideMenu.style.transition = 'transform 0.1s ease';
+
+      const autoJoinButton = subButton.cloneNode();
+      autoJoinButton.innerText = '自動参加\nモード';
+      autoJoinButton.style.background = '#ffb300';
+      autoJoinButton.style.color = '#000';
+      autoJoinButton.addEventListener('click',()=>{
+        autoJoinDialog.showModal();
+        if (!settings.teamColor) {
+          autoJoinSettingsDialog.showModal();
+        } else {
+          startAutoJoin();
+        }
+      })
+
+      const autoJoinDialog = document.createElement('dialog');
+      autoJoinDialog.style.background = '#fff';
+      autoJoinDialog.style.color = '#000';
+      autoJoinDialog.style.width = '90vw';
+      autoJoinDialog.style.height = '90vh';
+      autoJoinDialog.style.fontSize = '80%';
+      autoJoinDialog.style.textAlign = 'center';
+      autoJoinDialog.style.marginTop = '2vh';
+      autoJoinDialog.classList.add('auto-join');
+      document.body.append(autoJoinDialog);
+
+      const autoJoinSettingsDialog = document.createElement('dialog');
+      autoJoinSettingsDialog.style.background = '#fff';
+      autoJoinSettingsDialog.style.color = '#000';
+      autoJoinSettingsDialog.style.textAlign = 'left';
+      autoJoinDialog.append(autoJoinSettingsDialog);
+
+      (()=>{ // autoJoinSettingsDialog
+        const div = document.createElement('div');
+        const input = document.createElement('input');
+        const label = document.createElement('label');
+        label.style.display = 'flex';
+        label.style.width = '100%';
+        const span = document.createElement('span');
+        span.style.whiteSpace = 'nowrap';
+        span.style.width = '50%';
+        const span2 = document.createElement('span');
+        span2.style.width = '50%';
+        const closeButton = document.createElement('button');
+        closeButton.type = 'button';
+        closeButton.textContent = 'OK';
+        closeButton.addEventListener('click',()=>{
+          autoJoinSettingsDialog.close();
+          startAutoJoin();
+        })
+
+        const inputs = {
+          teamName: [input.cloneNode(),'チーム名'],
+          teamColor: [input.cloneNode(),'チームカラー']
+        }
+        for (const key of Object.keys(inputs)) {
+          const label_ = label.cloneNode();
+          const span_ = span.cloneNode();
+          const span2_ = span2.cloneNode();
+          span_.textContent = inputs[key][1];
+          span2_.append(inputs[key][0]);
+          label_.append(span_, span2_);
+          div.append(label_);
+
+          inputs[key][0].value = settings[key] || '';
+          inputs[key][0].addEventListener('input', ()=>{
+            inputs.teamColor[0].value = inputs.teamColor[0].value.replace(/[^0-9a-fA-F]/g,'');
+            settings[key] = inputs[key][0].value;
+            localStorage.setItem('aat_settings',JSON.stringify(settings));
+          })
+        }
+
+        const description = document.createElement('p');
+        description.style.fontSize = '90%';
+        description.innerText = 'チームカラーは小文字/大文字も正確に入力してください。（自陣の隣接タイル取得に必要）\nあらかじめ装備パネルからエリートも含め各ランクの装備を登録してください。（所持していない場合は除く）\n※装備を登録していないと成功率が低下します。'
+        div.append(description,closeButton);
+        autoJoinSettingsDialog.append(div);
+      })();
+
+      //autoJoin
+      (()=>{
+        const container = document.createElement('div');
+        container.style.display = 'flex';
+        container.style.flexDirection = 'column';
+        container.style.height = '100%';
+        container.style.color = '#000';
+
+        const log = document.createElement('div');
+        log.style.margin = '2px';
+        log.style.border = 'solid 1px #000';
+        log.style.overflow = 'auto';
+        log.style.flexGrow = '1';
+        log.style.textAlign = 'left';
+        log.classList.add('auto-join-log');
+
+        const settingsButton = document.createElement('button');
+        settingsButton.textContent = '設定';
+        settingsButton.addEventListener('click', ()=>{
+          autoJoinSettingsDialog.showModal();
+          clearInterval(autoJoinIntervalId);
+        })
+        const closeButton = document.createElement('button');
+        closeButton.style.fontSize = '100%';
+        closeButton.textContent = '自動参加モードを終了';
+        closeButton.addEventListener('click', ()=>{
+          autoJoinDialog.close();
+        })
+        const p = document.createElement('p');
+        p.textContent = 'この画面を開いたままにしておくこと';
+        p.style.margin = '0';
+
+        container.append(log, p, settingsButton, closeButton);
+        autoJoinDialog.append(container);
+      })();
+
+      const settingsButton = subButton.cloneNode();
+      settingsButton.textContent = '設定';
+      settingsButton.style.background = '#ffb300';
+      settingsButton.style.color = '#000';
+      settingsButton.addEventListener('click', ()=>{
+        settingsDialog.show();
+      })
+
+      const rangeAttackButton = subButton.cloneNode();
+      rangeAttackButton.textContent = '範囲攻撃';
+      rangeAttackButton.style.background = '#f64';
+      rangeAttackButton.style.color = '#fff';
+      rangeAttackButton.addEventListener('click', ()=>{
+        slideMenu.style.transform = 'translateX(-100%)';
+        cellSelectorActivate = true;
+      })
+
+      const closeSlideMenuButton = subButton.cloneNode();
+      closeSlideMenuButton.textContent = 'やめる';
+      closeSlideMenuButton.style.background = '#888';
+      closeSlideMenuButton.style.color = '#fff';
+      closeSlideMenuButton.addEventListener('click', ()=>{
+        slideMenu.style.transform = 'translateX(0)';
+        cellSelectorActivate = false;
+      })
+
+      const startRangeAttackButton = subButton.cloneNode();
+      startRangeAttackButton.textContent = '攻撃開始';
+      startRangeAttackButton.style.background = '#f64';
+      startRangeAttackButton.style.color = '#fff';
+      startRangeAttackButton.addEventListener('click', async()=>{
+        rangeAttackProcessing = true;
+        rangeAttackQueue.length = 0;
+        switchRangeAttackButtons();
+        await rangeAttack();
+        rangeAttackProcessing = false;
+        switchRangeAttackButtons();
+      })
+
+      const pauseRangeAttackButton = subButton.cloneNode();
+      pauseRangeAttackButton.textContent = '中断';
+      pauseRangeAttackButton.style.background = '#888';
+      pauseRangeAttackButton.style.color = '#fff';
+      pauseRangeAttackButton.addEventListener('click', ()=>{
+        if (!rangeAttackProcessing) return;
+        rangeAttackProcessing = false;
+        switchRangeAttackButtons();
+      })
+
+      const resumeRangeAttackButton = subButton.cloneNode();
+      resumeRangeAttackButton.textContent = '再開';
+      resumeRangeAttackButton.style.background = '#f64';
+      resumeRangeAttackButton.style.color = '#fff';
+      resumeRangeAttackButton.style.display = 'none';
+      resumeRangeAttackButton.addEventListener('click', async()=>{
+        rangeAttackProcessing = true;
+        switchRangeAttackButtons();
+        await rangeAttack();
+        rangeAttackProcessing = false;
+        switchRangeAttackButtons();
+      })
+
+      function switchRangeAttackButtons (){
+        if(rangeAttackProcessing) {
+          startRangeAttackButton.disabled = true;
+          resumeRangeAttackButton.style.display = 'none';
+          pauseRangeAttackButton.style.display = '';
+        } else {
+          startRangeAttackButton.disabled = false;
+          if (rangeAttackQueue.length > 0) {
+            resumeRangeAttackButton.style.display = '';
+            pauseRangeAttackButton.style.display = 'none';
+          }
+        }
+      }
+
+      const deselectButton = subButton.cloneNode();
+      deselectButton.textContent = '選択解除';
+      deselectButton.style.background = '#888';
+      deselectButton.style.color = '#fff';
+      deselectButton.addEventListener('click', ()=>{
+        const cells = document.querySelectorAll('.cell');
+        cells.forEach(cell => {
+          cell.classList.remove('selected');
+          cell.style.borderColor = '#ccc';
+        });
+      })
+
+      const batchSelectButton = subButton.cloneNode();
+      batchSelectButton.textContent = '一括選択';
+      batchSelectButton.style.background = '#ffb300';
+      batchSelectButton.style.color = '#000';
+      batchSelectButton.addEventListener('click', ()=>{
+        batchSelectMenu.style.display = 'flex';
+      })
+      const batchSelectMenu = document.createElement('div');
+      batchSelectMenu.style.display = 'none';
+      batchSelectMenu.style.flex = '1';
+      batchSelectMenu.style.justifyContent = 'center';
+      batchSelectMenu.style.gap = '2px';
+      batchSelectMenu.style.position = 'absolute';
+      batchSelectMenu.style.width = '100%';
+      batchSelectMenu.style.height = '100%';
+      batchSelectMenu.style.background = '#fff';
+
+      (()=>{
+        const ranks = ['N', 'R', 'SR', 'SSR', 'UR'];
+        ranks.forEach(rank=>{
+          const rankButton = subButton.cloneNode();
+          rankButton.style.width = '4.5em';
+          rankButton.style.background = '#ffb300';
+          rankButton.style.color = '#000';
+          rankButton.textContent = rank;
+          rankButton.addEventListener('click', ()=>{
+            const cells = document.querySelectorAll('.cell');
+            cells.forEach(cell => {
+              const cellRank = cell.querySelector('p')?.textContent || '';
+              const regex = new RegExp(`\\b${rank}(だけ)?e?$`);
+              const match = cellRank.match(regex);
+              if(match) {
+                cell.classList.add('selected');
+                cell.style.borderColor = '#f64';
+              } else {
+                cell.classList.remove('selected');
+                cell.style.borderColor = '#ccc';
+              }
+              batchSelectMenu.style.display = 'none';
+            })
+          })
+          batchSelectMenu.append(rankButton);
+        })
+        const closeButton = subButton.cloneNode();
+        closeButton.style.width = '4.5em';
+        closeButton.style.background
