2c7e84f434c5736d1d88ea185178fe70dd52c0d8
tetsuyadanbara <39230388+tetsuyadanbara@users.noreply.github.com>
2026-03-08 16:02:06 +0900

    Update ChestOpenerPROMAX2.js

--- ChestOpenerPROMAX2.js
+++ ChestOpenerPROMAX2.js
@@ -654,11 +654,6 @@
                         if (lockLink && lockLink.href.includes('/lock/')) {
                             lockPromises.push(fetch(lockLink.href));
                         }
-                    } else {
-                        const recycleLink = row.querySelectorAll('a')[3];
-                        if (recycleLink && recycleLink.href.includes('/recycle/')) {
-                            lockPromises.push(fetch(recycleLink.href));
-                        }
                     }
                 }
             }
