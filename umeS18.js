43d18f1d338a1179c49219540d3a58d4c03e0399
tetsuyadanbara <39230388+tetsuyadanbara@users.noreply.github.com>
2026-03-09 12:04:14 +0900

    Update umeS18.js

--- umeS18.js
+++ umeS18.js
@@ -2695,7 +2695,6 @@
 
       let regions = await getRegions();
       const excludeSet = new Set();
-      const isMorning = isMorningTime();
       let loop = 0;
 
       let cellType;
@@ -2713,6 +2712,7 @@
         let success = false;
         await new Promise(resolve => setTimeout(resolve, 0));
         isAutoJoinRunning = true;
+        const isMorning = isMorningTime();
 
         regions[cellType] = regions[cellType]
           .filter(e => !excludeSet.has(e.join(',')));
